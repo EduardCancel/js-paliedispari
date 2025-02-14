e350b5965ef767377e63131be7990ad6158d8477
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-14 17:29:51 +0100

    complete task

--- Pari-E-Dispari/main.js
+++ Pari-E-Dispari/main.js
@@ -1,15 +1,31 @@
 // Choose even or odd
-const userChoice = prompt("Choose even or odd")
+const userChoice = prompt("Choose even or odd");
 
 // Chose the number
-const userNumb = Number(prompt("Choose a number from 1 to 5"))
+const userNumb = Number(prompt("Choose a number from 1 to 5"));
 
-// Generate a random number 1 to 5
-const randomNumber = Math.ceil(Math.random() * 5)
+// generate a random number
 
-// Outuput
-const result = userNumb + randomNumber;
-console.log(`Your number is ${userNumb}, the number of pc is ${randomNumber}`);
-console.log(result);
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-// Loop for the even or odd
+const pcNumb = getRandomNumber(1, 5);
+console.log(pcNumb);
+
+function isEven(num1, num2) {
+    const sum = num1 + num2;
+
+    if (sum % 2 === 0) {
+    return true;
+}
+    return false;
+}
+
+if (isEven(pcNumb, userNumb) && userChoice == "pari") {
+    alert("yuo win!");
+} else if (isEven(pcNumb, userNumb) == false && userChoice == "dispari") {
+    alert("yuo win!");
+} else {
+    alert("You lose :( try again");
+}
